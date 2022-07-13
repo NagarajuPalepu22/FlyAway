ee4d5644141c314a3acf28176d64eed419c8f000
NagarajuPalepu22 <107348585+NagarajuPalepu22@users.noreply.github.com>
2022-07-13 10:37:00 +0530

    my second project

---
+++ src/main/java/com/flyaway/dao/DBConnect.java
@@ -0,0 +1,34 @@
+package com.flyaway.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+
+	public static Connection getConnection() throws SQLException {
+
+		Connection con = null;
+		String jdbcUrl = "jdbc:mysql://localhost:3306/flyaway";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String user = "root";
+		String password = "Nag@Raju9652";
+
+
+		try
+		{
+			Class.forName(driver);
+			con = DriverManager.getConnection(jdbcUrl, user, password);
+
+		}catch(Exception e)
+		{
+
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+
+
+}
